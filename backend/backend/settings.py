b166897f266d93452f9b2fa65a30b98ea1e1afa0
nesrinealoulou <nessrine.aloulou@enis.tn>
2024-09-11 10:10:08 +0100

    first commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,13 +96,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'enis_tp',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3307,
     }
 }
 
